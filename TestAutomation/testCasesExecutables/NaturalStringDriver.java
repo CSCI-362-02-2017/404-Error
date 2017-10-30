f0bea294654d37682df252ecf4cc75508770fc85
schwarzmb <schwarz1016@gmail.com>
2017-10-30 13:13:56 -0400

    Update NaturalStringDriver.java

--- TestAutomation/testCasesExecutables/NaturalStringDriver.java
+++ TestAutomation/testCasesExecutables/NaturalStringDriver.java
@@ -4,7 +4,7 @@ import java.text.Collator;
 import java.util.Comparator;
 
 public class NaturalStringDriver{
-	public static void main(String args[]){
+	public static void main(String[] args){
 		NaturalStrings testNS = new NaturalStrings();
 		String[] temp = args[0].split(", ");
 		String s = temp[0];
