0bed9319a75ae4cd32166109b0bce043b3491404
schwarzmb <schwarz1016@gmail.com>
2017-10-30 22:35:59 -0400

    Add files via upload

---
+++ TestAutomation/project/openmrs-core/api/src/main/java/org/openmrs/util/NaturalStringDriver.java
@@ -0,0 +1,16 @@
+package org.openmrs.util;
+
+import java.text.Collator;
+import java.util.Comparator;
+//import org.openmrs.util.*;
+
+public class NaturalStringDriver{
+	public static void main(String args[]){
+		NaturalStrings testNS = new NaturalStrings();
+		String[] temp = args[0].split(", ");
+		String s = temp[0];
+		String t = temp[1];
+		System.out.println(Integer.valueOf(testNS.compareNaturalAscii(s, t)));
+	}
+}
+		
