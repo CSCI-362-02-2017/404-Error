6d5969e71a40ea4ecb5056c84ae5971b5d233f0a
Matthew Kay <Mkay0961@gmail.com>
2017-11-19 16:26:53 -0500

    Adding Html Table

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -4,25 +4,52 @@
 cd ../testCases
 
 #Create report
+
+#
+##echo "<!DOCTYPE html>
+##<html>
+##	<head>
+#		<meta charset="UTF-8">
+#		<title>openMRS Tests</title>
+#		<link rel=\"stylesheet\" type=\"text/css\" href=\"../reports/style.css\" />
+#	</head>
+#	<body>
+#		<div id=\"table\">
+#			<div class=\"row\">
+#				<div class=\"head\">TestCase ID</div>
+#				<div class=\"head\">Requirement</div>
+#				<div class=\"head\">Method</div>
+#				<div class=\"head\">Class</div>
+#				<div class=\"head\">Driver</div>
+#				<div class=\"head\">Inputs</div>
+#				<div class=\"head\">Expected Output</div>
+#				<div class=\"head\">Result</div>
+#			</div>" > ../reports/testReport.html
+
+
 echo "<!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>openMRS Tests</title>
-		<link rel=\"stylesheet\" type=\"text/css\" href=\"../reports/style.css\" />
-	</head>
-	<body>
-		<div id=\"table\">
-			<div class=\"row\">
-				<div class=\"head\">TestCase ID</div>
-				<div class=\"head\">Requirement</div>
-				<div class=\"head\">Method</div>
-				<div class=\"head\">Class</div>
-				<div class=\"head\">Driver</div>
-				<div class=\"head\">Inputs</div>
-				<div class=\"head\">Expected Output</div>
-				<div class=\"head\">Result</div>
-			</div>" > ../reports/testReport.html
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
+<table style="width:100%">
+  <tr>
+    <th>Test</th>
+    <th>Requirement</th> 
+    <th>Method</th>
+    <th>Class</th> 
+    <th>Driver</th>
+    <th>Inputs</th> 
+    <th>Expected Output</th>
+    <th>Result</th>
+  </tr>" > ../reports/testReport.html
+
 #echo $PWD
 javac -cp . ../project/openmrs-core/api/src/main/java/org/openmrs/util/NaturalStrings.java ../project/openmrs-core/api/src/main/java/org/openmrs/util/NaturalStringDriver.java ../project/openmrs-core/api/src/main/java/org/openmrs/util/NaturalStringDriver2.java
 #echo $PWD
@@ -40,12 +67,14 @@ for file in ../testCases/*; do
 	expOutput=$(sed -n '17p' "$file")
 
 	#Run the correct driver
-	#echo "driver is: $driver"
+
 	cd ../project/openmrs-core/api/src/main/java
 	result=$(java -cp . $driver "$inputs")
 	cd ../../../../..
 
 	#Dump test case info into terminal
+	
+
 	echo "---------------------"
 	echo "Test: $test "
 	echo "---------------------"
@@ -56,32 +85,36 @@ for file in ../testCases/*; do
 	echo "Inputs: $inputs"
 	echo "Expected Output: $expOutput"
 	echo "Result: $result"
-	echo ""
+	
 
-	#Print results to report
-	echo "			<div class=\"row\">
-				<div class=\"cell\"><a href=\"../testCases/$file\">Test #$test</a></div>
-				<div class=\"cell\">$method</div>
-				<div class=\"cell\">$input</div>
-				<div class=\"cell\">$expected</div>
-				<div class=\"cell\">$result</div>" >> ../reports/testReport.html
+
+	echo "<tr>">> ../reports/testReport.html
+	echo "<td><a href=\"../testCases/$file\">Test #$test</a></td>">> ../reports/testReport.html
+	echo "<td>$req</td>">> ../reports/testReport.html
+	echo "<td>$method</td>">> ../reports/testReport.html
+	echo "<td>$class</td>">> ../reports/testReport.html
+	echo "<td>$driver</td>">> ../reports/testReport.html
+	echo "<td>$inputs</td>">> ../reports/testReport.html
+	echo "<td>$expOutput</td>">> ../reports/testReport.html
+
+	
 
 	#Compare the output to expected and determine pass or fail
 	if [ "$expected == $result" ]
 	 then
-		echo "				<div class=\"cell pass\">Pass</div>" >> ../reports/testReport.html
+		echo "<td>Pass</td>" >> ../reports/testReport.html
 	else
-		echo "				<div class=\"cell fail\">Fail</div>" >> ../reports/testReport.html
+		echo "<td>Fail</td>" >> ../reports/testReport.html
 	fi
 
 
-	echo "			</div>" >> ../reports/testReport.html
+	echo "</tr>" >> ../reports/testReport.html
 
 done
 
-echo "		</div>
-	</body>
+echo "</table>
+</body>
 </html>" >> ../reports/testReport.html
 
 #Open report
-xdg-open ../reports/testReport.html
+open ../reports/testReport.html
